59291bf3bc5475d2e027e17e12feb05441f5e3dc
azkall <120769795+azkazkiya@users.noreply.github.com>
2023-09-17 21:56:19 +0700

    Update no2.1.py

--- AzkalAzkiyaAlfiandri/no2.1.py
+++ AzkalAzkiyaAlfiandri/no2.1.py
@@ -1,13 +1,13 @@
 def f(x):
-    return x**3 - 2*x + 1
+    return x**3 - 2*x + 1 #fungsi dari soal nomor 1a
 
 def solve_equation_n_iterations(n):
-    x = 0  # Nilai awal x
+    x = 0  # Nilai awal dari x
     iteration = 0  # Inisialisasi hitungan iterasi
 
-    while iteration < n:
-        x_new = x - f(x) / (3*x**2 - 2)  # Metode Newton-Raphson untuk mencari akar
-        if abs(x_new - x) < 1e-6:  # Kondisi berhenti jika perbedaan sangat kecil
+    while iteration < n: #program perulangan
+        x_new = x - f(x) / (3*x**2 - 2)  # Metode untuk mencari akar
+        if abs(x_new - x) < 1e-6:  # Kondisi dimana program akan berhenti jika perbedaan sangat kecil
             break
         x = x_new
         iteration += 1
