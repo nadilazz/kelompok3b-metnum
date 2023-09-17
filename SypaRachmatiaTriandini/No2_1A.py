470f11b2526e553868ed64c2e68c246ff1c4a5b3
Sypartaa <144791179+Sypartaa@users.noreply.github.com>
2023-09-17 22:07:49 +0700

    Add files via upload

---
+++ SypaRachmatiaTriandini/No2_1A.py
@@ -0,0 +1,19 @@
+def f(x):
+    return x**3 - 2*x + 1 # Dari soal 1A
+
+def solve_equation_n_iterations(n):
+    x = 0  # Nilai x pada awalnya
+    iteration = 0  # Inisialisasi iterasi
+
+    while iteration < n: #perulangan iterasi kurang dari n
+        x_new = x - f(x) / (3*x**2 - 2)  # Metod untuk mencari akar
+        if abs(x_new - x) < 1e-6:  # Kondisi berhenti jika perbedaan sangat kecil
+            break
+        x = x_new
+        iteration += 1
+
+    return x
+
+n = 5  # Ganti dengan nilai n yang diinginkan user
+root = solve_equation_n_iterations(n)
+print(f"Akar setelah {n} iterasi adalah: {root}")
